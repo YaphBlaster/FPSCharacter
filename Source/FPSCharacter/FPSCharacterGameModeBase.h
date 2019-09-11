2bccdfebef81a575cf230ee4338057e6676067bb
YaphBlaster <find.mr.y@gmail.com>
2019-09-11 10:25:56 -0500

    Code Dump

---
+++ Source/FPSCharacter/FPSCharacterGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSCharacterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSCHARACTER_API AFPSCharacterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
